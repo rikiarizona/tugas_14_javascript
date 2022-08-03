e3f78000c59a5f56d4193ab58880a77c6659eefd
rikiarizona <rikiarizona@gmail.com>
2022-08-03 09:53:29 +0700

    TUGAS 14 JAVASCRIPT
    saya kirimkan tugas niomic

---
+++ index.js
@@ -0,0 +1,9 @@
+function hasilMat() {
+    var x = 37;
+    var tanggallahir = 27;
+    console.log(x/tanggallahir);
+    console.log(Math.round(x/tanggallahir));
+    console.log(Math.ceil(x/tanggallahir));
+    console.log(Math.floor(x/tanggallahir));
+}
+hasilMat()
